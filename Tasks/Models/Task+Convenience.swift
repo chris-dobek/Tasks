f6ea18b25b21bd6019912aa23c69b7a599e1902e
Ben Gohlke <jcgohlke@me.com>
2020-04-20 12:49:10 -0400

    Initial commit

---
+++ Tasks/Models/Task+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Task+Convenience.swift
+//  Tasks
+//
+//  Created by Ben Gohlke on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Tasks/View Controllers/CreateTaskViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateTaskViewController.swift
+//  Tasks
+//
+//  Created by Ben Gohlke on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
